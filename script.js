ce06558dbf1a662e230bbd7a7f4803fb520c28ae
Foqz <fikskamil@gmail.com>
2018-11-06 18:37:16 +0100

    FrontEndCleanup3

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-  // const apiRoot = 'https://guarded-ravine-55549.herokuapp.com/v1/task/';
-  // const trelloApiRoot = 'https://guarded-ravine-55549.herokuapp.com/v1/trello/';
-    const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://guarded-ravine-55549.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://guarded-ravine-55549.herokuapp.com/v1/trello/';
+  // const apiRoot = 'http://localhost:8080/v1/task/';
+  // const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
